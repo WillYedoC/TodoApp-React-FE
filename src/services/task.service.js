9ecf23a8b333cd0fcde2962890d901643fbb7aff
WillYedoC <169630283+WillYedoC@users.noreply.github.com>
2026-02-26 15:31:02 -0400

    TF-5: Correcciones

--- src/services/task.service.js
+++ src/services/task.service.js
@@ -1,24 +1,10 @@
-const API_URL = 'http://localhost:8000/api';
-
-const getAuthHeader = () => {
-  const token = localStorage.getItem('token');
-  return {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Authorization': `Bearer ${token}`,
-  };
-};
-
+import API_URL from './index';
 export const tareaService = {
   async getAll() {
-    const response = await fetch(`${API_URL}/tasks`, {
-      method: 'GET',
-      headers: getAuthHeader(),
-    });
-
-    if (!response.ok) {
+    const response = await fetch(`${API_URL}/tasks`);
+if (!response.ok) {
       throw new Error('Error al obtener las tareas');
     }
     return await response.json();
   },
-};
+}
